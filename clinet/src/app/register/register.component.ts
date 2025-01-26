f1e12db18d7ce0521017e962b0c2fb9bc4cda789
zanbary34 <zanbary34@gmail.com>
2025-01-26 16:44:38 +0200

    Update Registeration

---
+++ clinet/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, inject, Input, input, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+
+export class RegisterComponent {
+  private accountService = inject(AccountService);
+  model: any = {};
+  cancelRegister = output<boolean>();
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
